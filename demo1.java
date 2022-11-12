8ca3ec862382b12370fd2d4d4000c61b1e744b46
HellTractor13 <117569818+HellTractor13@users.noreply.github.com>
2022-11-12 22:28:35 +0800

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("输入两个数：");
+        Scanner sc=new Scanner(System.in);
+        int number1=sc.nextInt();
+        int number2=sc.nextInt();
+        System.out.println(number1+number2);
+
+
+    }
+}
